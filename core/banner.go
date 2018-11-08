f57393769c18d8b64c98a46d1821418615920259
Isaiah Schisler <isaiah.schisler@gmail.com>
2018-11-07 20:46:37 -0800

    change banner

--- core/banner.go
+++ core/banner.go
@@ -6,11 +6,5 @@ const (
 	Author      = "Isaiah Schisler"
 	Website     = "https://github.com/VerveWireless/sysops-tools/blob/master/aws/aws_cli_framework"
 	ASCIIBanner = "        _ __           __\n" +
-		"____   ____                         \n" +
-		"\\   \\ /   /______________  __ ____ \n" +
-		" \\   Y   // __ \\_  __ \\  \\/ // __ \\ \n" +
-		" \\     /\\  ___/|  | \\/\\   /\\  ___/\n" +
-		"   \\___/  \\___  >__|    \\_/  \\___  >\n" +
-		"              \\/                 \\/ \n" +
 		"         \\/ by @ischisler          \n"
 )
